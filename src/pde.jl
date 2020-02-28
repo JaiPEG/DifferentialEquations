c5f42a39779cbda100e1ca3a52a7aef62189d530
Cristian Ardelean <cristian97.ardelean@gmail.com>
2020-02-28 17:22:22 -0500

    Add wave equation solvers

--- src/pde.jl
+++ src/pde.jl
@@ -1,3 +1,5 @@
+include("function.jl")
+
 """
 Neumann boundary conditions on a 1-dimensional domain.
 """
@@ -87,4 +89,54 @@ function diffusionD(ode, bc::Dirichlet, f::Hat{R, a, b, n}, h::R)::Hat{R, a, b, 
 	g = ode(deriv2, f, h)
 	dirichlet!(bc, g)
 	g
+end
+
+"""
+Evolve a state vector by one time-step under the wave equation using the
+given autonomous ode solver.
+
+ode :: ((V -> V), V, R) -> R
+	Autonomous ODE solver
+	E.g.: rk2
+s :: Vector{V}
+	State vector.
+	First element is the time derivative of the solution at time t.
+	Second element is the space derivative of the solution at time t.
+h :: R
+	Time-step
+-> :: Vector{V}
+	State vector at time t + h
+"""
+function wave(ode, s::Vector{V}, h::R)::Vector{V} where {R, V}
+	rhs(s) = [deriv(s[2]), deriv(s[1])]
+	ode(rhs, s, h)
+end
+
+"""
+Evolve a state vector by one time-step under the wave equation using the
+given autonomous ode solver and apply fixed Dirichlet boundary conditions.
+Specifically, set the endpoints of the time deriviative of the solution
+to zero.
+
+ode :: ((Hat{R, a, b, n} -> Hat{R, a, b, n}), Hat{R, a, b, n}, R) -> R
+	Autonomous ODE solver
+	E.g.: rk2
+s :: Vector{Hat{R, a, b, n}}
+	State vector.
+	First element is the time derivative of the solution at time t.
+	Second element is the space derivative of the solution at time t.
+h :: R
+	Time-step
+-> :: Vector{Hat{R, a, b, n}}
+	State vector at time t + h
+"""
+function waveD0(ode, s::Vector{Hat{R, a, b, n}}, h::R)::Vector{Hat{R, a, b, n}} where {R, a, b, n}
+	newS = wave(ode, s, h)
+	# Set time derivative of solution to zero at the endpoints.
+	# Essentially apply a fixed Dirichlet boundary condition on the
+	# original solution.
+	# Idea by Benjamin de Bruyne.
+	newS[1].coeffs[1] = 0.0
+	newS[1].coeffs[end] = 0.0
+	newS
 end
